8df356b5ed107ebbb54c71067ac7b8dd9fad65e5
ItsMurumba <kevmurumba@gmail.com>
2022-12-07 14:19:16 -0500

    Added Service provider

--- src/HostpinnacleServiceProvider.php
+++ src/HostpinnacleServiceProvider.php
@@ -3,7 +3,40 @@
 namespace Itsmurumba\Hostpinnacle;
 
 use Illuminate\Support\ServiceProvider;
+use Itsmurumba\Hostpinnacle\Hostpinnacle;
 
 class HostpinnacleServiceProvider extends ServiceProvider
 {
+    /**
+     * Publishes all the config file this package needs to function
+     */
+    public function boot()
+    {
+        $config = realpath(__DIR__ . '/../resources/config/hostpinnacle.php');
+
+        $this->publishes([
+            $config => config_path('hostpinnacle.php')
+        ]);
+    }
+
+    /**
+     * Register the application services
+     */
+    public function register()
+    {
+        $this->app->bind('laravel-hostpinnacle', function () {
+
+            return new Hostpinnacle;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider
+     * @return array
+     */
+    public function provides()
+    {
+
+        return ['laravel-hostpinnacle'];
+    }
 }
