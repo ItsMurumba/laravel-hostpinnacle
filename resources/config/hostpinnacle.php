8b60ffe57e7b34ca6f1fc4f4479cdd6e9faa3924
ItsMurumba <kevmurumba@gmail.com>
2022-12-07 13:07:22 -0500

    Add Config file

--- resources/config/hostpinnacle.php
+++ resources/config/hostpinnacle.php
@@ -1,9 +1,28 @@
 <?php
 
 return [
+    /**
+     * API Key From Hostpinnacle Portal
+     */
+    'apiKey' => getenv('HOSTPINNACLE_API_KEY'),
 
+    /**
+     * Sender ID from Hostpinnacle Portal
+     */
+    'senderId' => getenv('HOSTPINNACLE_SENDER_ID'),
+
+    /**
+     * Username for logging into Hostpinnacle Portal
+     */
+    'username' => getenv('HOSTPINNACLE_LOGIN_USERNAME'),
+
+    /**
+     * Password for logging into Hostpinnacle Portal
+     */
+    'password' => getenv('HOSTPINNACLE_LOGIN_PASSWORD'),
+
+    /**
+     * Base URL for Hostpinnacle API
+     */
+    'baseUrl' => getenv('HOSTPINNACLE_BASE_URL'),
 ];
-
-
-
-
