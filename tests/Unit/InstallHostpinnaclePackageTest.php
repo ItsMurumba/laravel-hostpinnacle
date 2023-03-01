4dc861875502e422fa4708f401248ab6c30fcc70
ItsMurumba <kevmurumba@gmail.com>
2023-03-01 15:25:37 -0500

    Updated sampel test

--- tests/Unit/InstallHostpinnaclePackageTest.php
+++ tests/Unit/InstallHostpinnaclePackageTest.php
@@ -2,8 +2,28 @@
 
 namespace Itsmurumba\Hostpinnacle\Tests\Unit;
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Artisan;
 use Itsmurumba\Hostpinnacle\Tests\TestCase;
 
 class InstallHostpinnaclePackageTest extends TestCase
 {
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function the_install_command_copies_the_configuration()
+    {
+        if (File::exists(config_path('hostpinnacle.php'))) {
+            unlink(config_path('hostpinnacle.php'));
+        }
+
+        $this->assertFalse(File::exists(config_path('hostpinnacle.php')));
+
+        Artisan::call('hostpinnacle:install');
+
+        $this->assertTrue(File::exists(config_path('hostpinnacle.php')));
+    }
 }
